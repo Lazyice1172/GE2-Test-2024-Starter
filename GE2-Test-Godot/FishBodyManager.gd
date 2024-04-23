5374bcf6bd5442ad9f884c13b82e22e6f1495717
Lazyice1172 <78085794+Lazyice1172@users.noreply.github.com>
2024-04-23 11:20:44 +0100

    gizmo size change

--- GE2-Test-Godot/FishBodyManager.gd
+++ GE2-Test-Godot/FishBodyManager.gd
@@ -1,13 +1,18 @@
 extends Node3D
 
 
-var length:= 5.0
-var tail:= 2.0
+var basic_length:= 5.0
+var basic_tail:= 2.0
 var swing:= 1.0
 
-@export var fishBody:PackedScene
+var random_size := randf_range(0.5, 2.0)
+var length:= basic_length * random_size
+var tail:= basic_tail * random_size
 
-@onready var fish_body_manager: Node3D = $FishBodyManager
+@export var gizmoBody:PackedScene
+@export var gizmotail:PackedScene
+
+@onready var gizmo_body_manager: Node3D = $FishBodyManager
 @onready var gizmo: CharacterBody3D = $".."
 
 
@@ -15,15 +20,25 @@ func _process(delta: float) -> void:
 	#Try to create fish body
 	
 	if length > 0:
-		var new_fishBody = fishBody.instantiate()
-		add_child(new_fishBody)
-		new_fishBody.global_position.x = gizmo.global_position.x
-		new_fishBody.global_position.y = gizmo.global_position.y
-		new_fishBody.global_position.z = gizmo.global_position.z + length
+		var new_gizmoBody = gizmoBody.instantiate()
+		add_child(new_gizmoBody)
+		new_gizmoBody.global_position.x = gizmo.global_position.x
+		new_gizmoBody.global_position.y = gizmo.global_position.y
+		new_gizmoBody.global_position.z = gizmo.global_position.z + length
 		
 		
 		
 		length -= 1.0
+		
+	if tail > 0:
+		var new_gizmotail = gizmotail.instantiate()
+		add_child(new_gizmotail)
+		new_gizmotail.global_position.x = gizmo.global_position.x
+		new_gizmotail.global_position.y = gizmo.global_position.y
+		new_gizmotail.global_position.z = gizmo.global_position.z + length + tail
+		
+		tail -= 1.0
+	pass
 	pass
 
 func _physics_process(delta: float) -> void:
